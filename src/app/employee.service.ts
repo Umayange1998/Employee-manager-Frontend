21b39139730ca40ba5d9b657465120226f6b9525
Umayange1998 <wgikumayanga@gmail.com>
2025-05-23 01:09:02 +0530

    EmployeeService functions

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private apiServerUrl = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAllEmployees(): Observable <Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable <Employee>{
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`,employee);
+  }
+  
+  public updateEmployee(employee: Employee): Observable <Employee>{
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`,employee);
+  }
+  
+  public deleteEmployee(employeeId: Employee): Observable <Employee>{
+    return this.http.delete<Employee>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
 }
